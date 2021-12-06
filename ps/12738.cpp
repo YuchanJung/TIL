8b006163504f6ae4e5dc298816788813c86dfa2c
yuchannn <yuchanbpp@gmail.com>
2021-12-06 22:36:31 +0900

    12738 AC(call by value -> 값 복사로 인해 O(N))

--- ps/12738.cpp
+++ ps/12738.cpp
@@ -3,57 +3,37 @@
 
 using namespace std;
 
-int binary_search(vector<int> record, int value) {
-    int start = 0;
-    int end = record.size() - 1;
-    while (start < end) {
-        int mid = (start + end) / 2;
-        if (value > record[mid]) {
-            start = mid + 1;
-        }
-        else {
-            end = mid;
-        }
-    }
-    return end;
-}
-
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
     int n, i;
     int res = 1;
     cin >> n;
     int arr[1000000];
     vector<int> record;
-    // vector<int> length;
     for (i = 0; i < n; i++) {
         cin >> arr[i];
     }
     record.push_back(arr[0]);
-    // length.push_back(1);
     for (i = 1 ; i < n; i++) {
         if (arr[i] > record.back()) {
             record.push_back(arr[i]);
-            /*
-            length.push_back(record.size());
-            if (res < length.back()) {
-                res = length.back();
-            }
-            */
         }
         else {
-            int idx = binary_search(record, arr[i]);
-            record[idx] = arr[i];
-            // length.push_back(idx + 1);
+            int start = 0;
+            int end = record.size() - 1;
+            while (start < end) {
+                int mid = (start + end) / 2;
+                if (arr[i] > record[mid]) {
+                    start = mid + 1;
+                }
+                else {
+                    end = mid;
+                }
+            }
+            record[end] = arr[i];
         }
     }
-    /*
-    for (i = 0; i < record.size(); i++) {
-        cout << record[i];
-    }
-    cout << endl;
-    for (i = 0; i < n; i++) {
-        cout << length[i];
-    }
-    */
     cout << record.size() << endl;
 }
