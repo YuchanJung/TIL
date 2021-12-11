d706e70bd4b3c154d097505c35a008eef48d6f87
yuchannn <yuchanbpp@gmail.com>
2021-12-11 22:46:19 +0900

    17144 AC

--- ps/17144.cpp
+++ ps/17144.cpp
@@ -6,13 +6,38 @@
 using namespace std;
 const int dx[] = {0, -1, 0, 1};
 const int dy[] = {1, 0, -1, 0};
-const int order[] = {0, 1, 2, 3};
-const int reverse[] = {0, 3, 2, 1};
+int r, c, t;
+
+void clean(int matrix[][50], deque<tuple<int, int, int>> &deq, int x, int y, int m1, int m2) {
+    int nx, ny;
+    nx = ny = -1;
+    if ((y == 0 && x > 0 && x < m1) || (y == c - 1 && x > m2 && x <= r - 1)) {
+        nx = x - 1;
+        ny = y;
+    }
+    else if ((x == 0 || x == r - 1) && y < c - 1) {
+        nx = x;
+        ny = y + 1;
+    }
+    else if ((x == m1 || x == m2) && y > 1) {
+        nx = x;
+        ny = y - 1;
+    }
+    else if ((y == 0 && x > m2 && x < r -1) || (y == c - 1 && x >= 0 && x < m1)) {
+        nx = x + 1;
+        ny = y;
+    }
+    // cout << nx << ny << endl;
+    if (nx != -1) {
+        matrix[x][y] = matrix[nx][ny];
+        matrix[nx][ny] = 0;
+        clean(matrix, deq, nx, ny, m1, m2);
+    }
+}
 
 
 int main() {
     int i, j;
-    int r, c, t;
     int matrix[50][50] = {0};
     vector<int> machine;
     deque<tuple<int, int, int>> deq;
@@ -44,15 +69,30 @@ int main() {
                 matrix[x][y] -= m / 5;
             }
         }
+        clean(matrix, deq, machine[0] - 1, 0, machine[0], machine[1]);
+        clean(matrix, deq, machine[1] + 1, 0, machine[0], machine[1]);
+        // debugging
+        for (i = 0; i < r; i++) {
+            for (j = 0; j < c; j++) {
+                if (matrix[i][j] > 4) {
+                    deq.push_back(make_tuple(i, j, matrix[i][j]));
+                }
+            }
+        }
+        /*
+        for (i = 0; i < r; i++) {
+            for (j = 0; j < c; j++) {
+                cout << matrix[i][j] << ' ';
+            }
+            cout << endl;
+        }
+        */
     }
-    // debugging
-    /*
+    int res = 0;
     for (i = 0; i < r; i++) {
         for (j = 0; j < c; j++) {
-            cout << matrix[i][j];
+            res += matrix[i][j];
         }
-        cout << endl;
     }
-    */
-    
+    cout << res + 2 << endl;
 }
